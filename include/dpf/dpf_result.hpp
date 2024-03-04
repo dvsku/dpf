926cd7e612de153016bc9c1be855ebd4ed6c711a
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-03-04 12:47:22 +0100

    Implement dpf_result

--- include/dpf/dpf_result.hpp
+++ include/dpf/dpf_result.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
-namespace dvsku::dpf {
-    struct dpf_result {
+#include <string>
 
+namespace dvsku::dpf {
+    enum class dpf_status : unsigned char {
+        undefined = 0x0,
+        cancelled = 0x1,
+        error     = 0x2,
+        finished  = 0x3
+    };
+
+    struct dpf_result {
+        dpf_status  status  = dpf_status::undefined;
+        std::string message = "";
     };
 }
